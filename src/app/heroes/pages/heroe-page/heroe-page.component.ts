c7b89ebcda4d8977e0849f4341392d8de2fb0d43
Agustin Farisano <farisanoagustin@gmail.com>
2024-02-23 14:37:08 -0300

    404 not found error route for users navigation flow

--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroe-page',
   templateUrl: './heroe-page.component.html',
   styleUrls: ['./heroe-page.component.css']
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit {
 
+  public hero?:Hero;
+  
+  constructor(
+    private heroesService:HeroesService,
+    private activateRoute:ActivatedRoute,
+    private router:Router
+  ){}
+
+  ngOnInit(): void {
+     this.activateRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroById(id)),
+      ).subscribe(hero => {
+        
+        if(!hero) return this.router.navigate(['/heroes/list']);
+
+           this.hero = hero;
+           return
+
+        console.log({hero});
+      })
+  }
+  
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { Observable, catchError, from, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { HttpClient } from '@angular/common/http';
 import { enviroments } from 'src/enviroments/enviroments';
@@ -14,6 +14,13 @@ export class HeroesService {
 
     getHeroes():Observable<Hero[]>{
         return this.http.get<Hero[]>(`${ this.baseURL }/heroes`);
+    }
+
+    getHeroById(id:string):Observable<Hero | undefined>{
+        return this.http.get<Hero>(`${ this.baseURL }/heroes/${id}`)
+         .pipe(
+            catchError(error => of(undefined))
+         )
     }
     
 }
