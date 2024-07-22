2e50121e4ac3717d97636f91a3d0efb43cc5bee8
Agustin Farisano <farisanoagustin@gmail.com>
2024-07-22 15:10:25 -0300

    add autentication method for users login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { enviroments } from 'src/enviroments/enviroments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -23,8 +23,24 @@ export class AuthService {
       return this.http.get<User>(`${ this.baseUrl }/users/1`)
                 .pipe(
                    tap( user => this.user = user),
-                   tap( user => localStorage.setItem('token', user.id.toString() )), 
+                   tap( user => localStorage.setItem('token', 'aAsassadasBSDAaha.ahajhaA123SDmmdko' )), 
                 );
+    }
+
+    checkAutentication():Observable<boolean>{
+        if( !localStorage.getItem('token') )return of(false);
+
+        const token = localStorage.getItem('token');
+
+        return this.http.get<User>(`${this.baseUrl}/users/1`)
+            .pipe(
+                tap (user => this.user = user),
+                map ( user => !!user),
+                catchError(err => of(false))
+            )
+
+
+        return of(true);
     }
 
     logout(){
